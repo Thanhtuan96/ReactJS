f489cc1b0cb47ba0279e0e5a710a6f819c66b8f0
FreeCoder <chip.ntt@gmail.com>
2019-02-17 00:56:30 +0900

    complete todo-list app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoItem from './comonents/TodoItem';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todoItems : [
+        {title:'Mua Bim Bim', isComplete: true},
+        {title:'Mua Quần áo', isComplete: false},
+        {title:'Đi đổ xăng'}
+      ]
+    };
+  }
+
+  onItemClicked(item){
+
+    return(event)=>{
+      const isComplete = item.isComplete;
+      const {todoItems } = this.state;
+      const index = todoItems.indexOf(item);
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          { 
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      });
+    }
+  }
+ 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { todoItems } = this.state;
+    if(todoItems.length){
+      return (
+        <div className="App">
+          {
+          todoItems.length && todoItems.map((item,index)=> 
+            <TodoItem 
+            key={index} 
+            item={item} 
+            onClick ={this.onItemClicked(item)}/>)
+          }
+        </div>
+      );
+    }
   }
 }
 
